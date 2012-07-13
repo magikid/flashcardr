522c42b9107505b72994eccad6822053936bf1c0
Chris W Jones <chris@christopherjones.us>
2012-07-13 04:11:05 -0800

    Forgot to end SQL statements with ;

--- flashcards/schema.sql
+++ flashcards/schema.sql
@@ -11,12 +11,12 @@ CREATE TABLE IF NOT EXISTS cards (
 	assoc_deck INTEGER NOT NULL
 );
 
-INSERT OR IGNORE INTO decks (id, deck_title, owner) VALUES (1, "Home", 1)
-INSERT OR IGNORE INTO decks (id, deck_title, owner) VALUES (2, "Air Force", 1)
-INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (1, "table", 1, 1)
-INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (2, "chair", 2, 1)
-INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (3, "coin", 1, 2)
-INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (4, "commander", 2, 2)
+INSERT OR IGNORE INTO decks (id, deck_title, owner) VALUES (1, "Home", 1);
+INSERT OR IGNORE INTO decks (id, deck_title, owner) VALUES (2, "Air Force", 1);
+INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (1, "table", 1, 1);
+INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (2, "chair", 2, 1);
+INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (3, "coin", 1, 2);
+INSERT OR IGNORE INTO cards (id, card_data, card_number, assoc_deck) VALUES (4, "commander", 2, 2);
 
 
 
